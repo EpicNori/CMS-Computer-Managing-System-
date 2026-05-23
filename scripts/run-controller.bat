a4c1c79c041bfe8d30bb5c344b1a54ecc1527bc1
EpicNori <noriel.jandl@gmail.com>
2026-05-23 15:25:36 +0200

    Publish installer for anonymous download

--- scripts/run-controller.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set "CMS_PS=%~dp0run-controller.ps1"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%CMS_PS%" %*
-set "CMS_EXIT=%ERRORLEVEL%"
-if not "%CMS_EXIT%"=="0" pause
-exit /b %CMS_EXIT%
